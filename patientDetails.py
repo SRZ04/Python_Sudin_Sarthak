58de94d408740415a5f0eccceba04b829f9335b9
Sudin Rajbhandari <85798040+SRZ04@users.noreply.github.com>
2022-11-26 18:11:52 +0545

    Add files via upload

--- patientDetails.py
+++ patientDetails.py
@@ -17,7 +17,7 @@ def patientDetailsFunction():
                     )
                     '''
             patientCursor.execute(create_table)
-        # createTable()
+    # createTable()
 
     def insertTable():
         #insert
@@ -47,52 +47,47 @@ def patientDetailsFunction():
                 print(f"Age: {data[1]}")
                 print(f"Gender: {data[2]}")
                 print(f"Emergency: {data[3]}")
-            conn.commit
-        
+            conn.commit        
     
 
     press=int(input("Press 2 to view details and 3 to log in new record: "))
-    try:
-        if press==2:
-                searchTable()
+    if press==2:
+                    searchTable()
 
-        if press==3:
-                insertTable()
-                searchTable()
-    except:
-        print("No doctors available.")
-    else:
-        x=input("Is there an emergency?")
-        if(x.lower()=="yes"):
-            print("Please refer to hotline number below: \n 1618-01-4288919")
-
+    if press==3:
+                    insertTable()
+                    searchTable()
+   
+                
     def searchDoctors():
-            select_cmd='''SELECT  * FROM drDetailsTable'''
-            result=doctorCursor.execute(select_cmd)
-            count=0
-            for data in result:
-                if(data[4].lower()=="yes"):
-                    count+=1
-                    if(count==1):
-                        print('The name of available doctors are:\n')
-                    print(f"Name:{data[0]}") 
-                    print(f"Contact: {data[1]}")
-                    print(f"Availability: {data[4]}")
-            return count
+                select_cmd='''SELECT  * FROM drDetailsTable'''
+                result=doctorCursor.execute(select_cmd)
+                count=0
+                for data in result:
+                    if(data[4].lower()=="yes"):
+                        count+=1
+                        if(count==1):
+                            print('The name of available doctors are:\n')
+                        print(f"Name:{data[0]}") 
+                        print(f"Contact: {data[1]}")
+                        print(f"Availability: {data[4]}")
+                return count
 
 
 
     choice=input('Do you want to check if doctors are available? type yes/no: ')
-    if(choice.lower()=="yes"):
-        try:        
-            if(searchDoctors()==0):
-                    em=input("Confirm emergency(y/n): ")
-                    if(em.lower()!='y'):
-                     print('Sorry,no doctors are available at the moment')
-                    
-        except:
-                if(em.lower()=='y'):
-                    print("Please refer to hotline number below: \n 1618-01-4288919")       
+    try:
+        if(choice.lower()=="yes"):                
+                if(searchDoctors()==0):
+                        print('Sorry,no doctors are available at the moment')
+    except:
+        print("No doctors available at the moment.")
+        x=input("Confirm emergency? ")
+        if(x.lower()=="yes"):
+            print("Please refer to hotline number refered below: \n 1618-01-4288919")
+                        
+        
+                
     
 
 
